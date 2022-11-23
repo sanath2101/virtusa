ed410d97d811bcc4c63dda66ed61058799d1f81c
sanath2101 <101169524+sanath2101@users.noreply.github.com>
2022-11-23 11:24:27 +0530

    Add files via upload

---
+++ test 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GFG
+{
+
+static String CHARS = "abcdefghijklmnopqrstuvwxyz";
+static int MAX = 26;
+
+static String getString(char[] str, int n)
+{
+	Dictionary<char,
+			char> uMap = new Dictionary<char,
+										char>();
+	for (int i = 0; i < MAX; i++)
+	{
+		if(!uMap.ContainsKey(CHARS[i]))
+			uMap.Add(CHARS[i], CHARS[(i + 3)%MAX]);
+		else
+			uMap[CHARS[i]] = CHARS[(i + 3)%MAX ];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		str[i] = uMap[str[i]];
+	}
+	return String.Join("", str);
+}
+
+public static void Main(String []args)
+{
+    Console.WriteLine("enter a string:");
+    string String = Console.ReadLine();
+	int n = String.Length;
+
+	Console.WriteLine(getString(String.ToCharArray(), n));
+}
+}
+
---
+++ test 1/README.txt
@@ -0,0 +1,13 @@
+Replace every character with the 3rd character from a given string.
+
+input 1: Enter a string: abcd
+
+output: defg
+
+input 2:Enter a string: xyz
+
+output: abc
+
+input 3: Enter a string: rsa
+
+output: uvd
